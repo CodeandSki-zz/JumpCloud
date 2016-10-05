04dec2a6c6375dad14b67433282de460d0c49c95
Zack Stickles <zack.stickles@jumpcloud.com>
2016-10-05 10:30:15 -0600

    Created a public directory for applicants to work in

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var
   app = express(),
   PORT = 3004;
 
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function() {
   console.log('Server started on port ' + PORT);
